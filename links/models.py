00bb497389c1c274e28e2cc90375482709528678
gball19 <39078772+gball19@users.noreply.github.com>
2021-04-06 14:08:55 -0400

    first commit

---
+++ links/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class link(models.Model):
+    hyperlink = models.CharField(max_length=200, default = '')
+    website_name = models.CharField(max_length=200, default= '')
+    def __str__(self):
+        return self.website_name
+    def __hyperlink__(self):
+        return self.hyperlink
