92ffdb5ce78fc5704bac638c6a90c45a06ee1577
gball19 <39078772+gball19@users.noreply.github.com>
2021-04-06 15:58:43 -0400

    Adding link page

--- links/models.py
+++ links/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class link(models.Model):
     hyperlink = models.CharField(max_length=200, default = '')
     website_name = models.CharField(max_length=200, default= '')
+    image = models.ImageField(upload_to='images/', blank=True)
     def __str__(self):
         return self.website_name
     def __hyperlink__(self):
--- links/views.py
+++ links/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-
+from .models import link
 # Create your views here.
+from .forms import ImageForm
 from django.http import HttpResponse
 
 
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the links index.")
+    link_list = link.objects.all()
+    context = {'link_list' : link_list}
+    return render(request, 'links/index.html', context)
+    
+
+def image_upload_view(request):
+    """Process images uploaded by users"""
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # Get the current instance object to display in the template
+            img_obj = form.instance
+            return render(request, 'index.html', {'form': form, 'img_obj': img_obj})
+    else:
+        form = ImageForm()
+    return render(request, 'index.html', {'form': form})
