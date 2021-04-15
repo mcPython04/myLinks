f45133160e60b840885e3767f6402286b26e2a6c
frogies44 <frogies44@Laptop-Grayson.localdomain>
2021-04-15 13:55:28 -0500

    Removed Nonexistant User Pages

--- links/views.py
+++ links/views.py
@@ -1,9 +1,8 @@
 #from .models import link
 # Create your views here.
 #from .forms import ImageForm
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
-from django.shortcuts import redirect
+from django.http import HttpResponse, Http404
+from django.shortcuts import redirect, render
 from .models import *
 from django.contrib.auth.models import User
 from django.template import loader
@@ -12,12 +11,18 @@ def home(request, username):
     return render(request, 'home.html')
     
 def user_page(request,username):
-    link_list = link.objects.all()
-    template = loader.get_template('user_page.html')
-    context = {
-        'link_list' : link_list,'username' : username
-    }
-    return HttpResponse(template.render(context, request))   
+    #TO DO: 
+    #If user does not exists, 404
+    if User.objects.filter(username=username).exists():
+    #Make default redirect option
+        link_list = link.objects.all()
+        template = loader.get_template('user_page.html')
+        context = {
+            'link_list' : link_list,'username' : username
+        }
+        return HttpResponse(template.render(context, request))   
+    else:
+        raise Http404("No Such User Exists.")
 
 def add_view(request): 
     """Process images uploaded by users"""
