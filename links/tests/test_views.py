b1587a0c031c13b310800cb46176d37dd62c5737
mcPython04 <chen1911893611@gmail.com>
2021-05-10 13:49:44 -0500

    Wrote test code to test urls

--- links/tests/test_views.py
+++ links/tests/test_views.py
@@ -1,24 +1,3 @@
 from django import urls
 from django.contrib.auth import get_user_model
 import pytest
-
-
-@pytest.mark.parametrize('param', [
-    ('home'),
-    ('base'),
-    ('userPage'),
-    ('updateLink'),
-    ('createLink'),
-    ('createCollection'),
-    ('deleteLink'),
-    ('uploadLink'),
-    ('detailCollection'),
-    ('deleteCollection'),
-    ('removeLink'),
-    ('updateCollection'),
-    ('collectionPage'),
-])
-def test_render_views(client, param):
-    temp_url = urls.reverse(param)
-    resp = client.get(temp_url)
-    assert resp.status_code == 200
