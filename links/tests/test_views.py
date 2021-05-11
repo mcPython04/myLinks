a147d9eb6597beb87d2070ee337419247ae49337
lmd414 <lmd414@msstate.edu>
2021-05-11 12:19:23 -0500

    Test for link create view

--- links/tests/test_views.py
+++ links/tests/test_views.py
@@ -5,7 +5,8 @@ from links.models import link
 from links.views import *
 from links.forms import *
 import pytest
-
+from django.urls import reverse
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 @pytest.mark.django_db
 def test_link_set_true(client, create_test_link):
@@ -14,18 +15,24 @@ def test_link_set_true(client, create_test_link):
     test_link = create_test_link
     client.force_login(test_link.user)
     form = UpdateLinkSetDefaultForm(data={'d_id': test_link.id, 'set': "Set", 'type': 'Default'})
-    #import pdb; pdb.set_trace()
-    req = client.post('/links/update', data=form.data)
+    req = client.post(reverse('updateLink'), data=form.data)
     assert req.status_code == 302
     test_link = link.objects.get(id=test_link.id)
     assert test_link.default == True
 
+
 #django.forms.widgets.linkForm
 @pytest.mark.django_db
-def test_link_create_view():
+def test_link_create_view(client, create_test_user):
+    user = create_test_user
+    client.force_login(user)
     lcv = LinkCreateView()
-    form = lcv.get_form_class(data={'hyperlink': , 'website_name': , 'image': 'Default'})
-
-    import pdb;pdb.set_trace()
-    pass
+    form = lcv.get_form_class()
+    newphoto = SimpleUploadedFile(name='test_image.jpg', content=open('/home/ldoan/Downloads/background.jpg', 'rb').read(),
+                                        content_type='image/jpeg')
+    form = form(data={'hyperlink': 'https://twitter.com/', 'website_name': 'Twitter', 'image': newphoto})
+    req = client.post(reverse('createLink'), data=form.data)
+    #import pdb;pdb.set_trace()
+    assert req.status_code == 302
+    assert link.objects.count() == 1
 
