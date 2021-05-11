cbbd97c5f7cb0ac19e77c09146fd05715d527299
lmd414 <lmd414@msstate.edu>
2021-05-11 12:45:41 -0500

    Added tests for other functionalities of update link view

--- links/tests/test_views.py
+++ links/tests/test_views.py
@@ -8,17 +8,51 @@ import pytest
 from django.urls import reverse
 from django.core.files.uploadedfile import SimpleUploadedFile
 
+
 @pytest.mark.django_db
-def test_link_set_true(client, create_test_link):
-    assert User.objects.count() == 1
-    assert link.objects.count() == 1
+def test_update_link_set_default_true(client, create_test_link):
     test_link = create_test_link
     client.force_login(test_link.user)
-    form = UpdateLinkSetDefaultForm(data={'d_id': test_link.id, 'set': "Set", 'type': 'Default'})
+    form = UpdateLinkSetDefaultForm(data={'d_id': test_link.id, 'set': 'Set', 'type': 'Default'})
     req = client.post(reverse('updateLink'), data=form.data)
     assert req.status_code == 302
     test_link = link.objects.get(id=test_link.id)
     assert test_link.default == True
+
+
+@pytest.mark.django_db
+def test_update_link_set_default_false(client, create_test_link):
+    test_link = create_test_link
+    test_link.default = True
+    client.force_login(test_link.user)
+    form = UpdateLinkRemoveDefaultForm(data={'d_id': test_link.id, 'set': 'Remove', 'type': 'Default'})
+    req = client.post(reverse('updateLink'), data=form.data)
+    assert req.status_code == 302
+    test_link = link.objects.get(id=test_link.id)
+    assert test_link.default == False
+
+
+@pytest.mark.django_db
+def test_update_link_set_enable_false(client, create_test_link):
+    test_link = create_test_link
+    client.force_login(test_link.user)
+    form = UpdateLinkDisableForm(data={'d_id': test_link.id, 'set': 'Disable', 'type': 'Enable'})
+    req = client.post(reverse('updateLink'), data=form.data)
+    assert req.status_code == 302
+    test_link = link.objects.get(id=test_link.id)
+    assert test_link.enabled == False
+
+
+@pytest.mark.django_db
+def test_update_link_set_enable_true(client, create_test_link):
+    test_link = create_test_link
+    test_link.enabled = False
+    client.force_login(test_link.user)
+    form = UpdateLinkDisableForm(data={'d_id': test_link.id, 'set': 'Set', 'type': 'Enable'})
+    req = client.post(reverse('updateLink'), data=form.data)
+    assert req.status_code == 302
+    test_link = link.objects.get(id=test_link.id)
+    assert test_link.enabled == True
 
 
 #django.forms.widgets.linkForm
@@ -29,7 +63,7 @@ def test_link_create_view(client, create_test_user):
     lcv = LinkCreateView()
     form = lcv.get_form_class()
     newphoto = SimpleUploadedFile(name='test_image.jpg', content=open('/home/ldoan/Downloads/background.jpg', 'rb').read(),
-                                        content_type='image/jpeg')
+                                  content_type='image/jpeg')
     form = form(data={'hyperlink': 'https://twitter.com/', 'website_name': 'Twitter', 'image': newphoto})
     req = client.post(reverse('createLink'), data=form.data)
     #import pdb;pdb.set_trace()
